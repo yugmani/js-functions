01fcd66a43f61f2fd74af9e30582ac96d3041344
yugmani <yugmani@yahoo.com>
2022-03-15 12:30:15 -0700

    function min

--- index.js
+++ index.js
@@ -6,21 +6,57 @@ import './style.css';
 // appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 //function with default parameter
-function showMessage(name, location = 'Los Angeles'){ // default for location
-  name = name || "Yug";
-    return `${name} lives in ${location}.`;
+// **********************************
+
+//default parameter
+function showMessage(name, location = 'Los Angeles') {
+  // default for location
+  name = name || 'Yug';
+  return `${name} lives in ${location}.`;
 }
 
 // function call
-console.log(showMessage("Tara", "Tokyo"));  //Tara lives in Tokyo.
-console.log(showMessage("Alan")); //Alan lives in Los Angeles.
-console.log(showMessage("","Kathmandu"));
+console.log(showMessage('Tara', 'Tokyo')); //Tara lives in Tokyo.
+console.log(showMessage('Alan')); //Alan lives in Los Angeles.
+console.log(showMessage('', 'Kathmandu'));
 
-function friendGreeting(text){
-  text = text || "Yug";
+function friendGreeting(text) {
+  text = text || 'Yug';
   return `Hello ${text}!`;
 }
 
-friendGreeting("Derren");
-friendGreeting();
+console.log(friendGreeting('Derren')); //with argument
+console.log(friendGreeting()); //without argument
 
+function countwise(count) {
+  // if count is undefined or null return 'invalid count'
+  return count ?? 'Invalid count!';
+}
+
+console.log(countwise(101)); //101
+console.log(countwise()); //Invalid count!
+
+// Rewrite the function using '?' or '||'
+// *******************************************
+
+function checkAge(age) {
+  age = age > 18 || confirm('Did parents allow you?');
+}
+
+function isAgeGreater(age) {
+  age = age > 18 ? true : confirm('Did parents allow you?');
+}
+
+// console.log(checkAge());
+// console.log(isAgeGreater(12));
+
+
+// Function min(a, b)
+// Write a function min(a,b) which returns the least of two numbers a and b.
+function min(a, b){
+  return a < b ? a : b;
+}
+
+console.log(min(2, 5)); //2
+console.log(min(1, 1)); //1
+console.log(min(3, -1)); //-1
